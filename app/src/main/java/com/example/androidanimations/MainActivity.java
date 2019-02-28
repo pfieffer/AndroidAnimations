c640e93e0c772eff14a38d58acbfad2248c5755e
pfieffer <cbpmgr@gmail.com>
2019-02-28 20:33:43 +0545

    Implement AnimatorSet

--- app/src/main/java/com/example/androidanimations/MainActivity.java
+++ app/src/main/java/com/example/androidanimations/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidanimations;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -22,12 +23,17 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator animatorX = ObjectAnimator.ofFloat(imageView, "scaleX", 1f, 2f)
                 .setDuration(1000);
 
-        animatorX.start();
-
         ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageView, "scaleY", 1f, 2f)
                 .setDuration(1000);
 
-        animatorY.start();
+        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.playTogether(animatorX, animatorY); //pass in animator objects separated by comma or a list of animator objects
+
+        animatorSet.playSequentially(animatorX, animatorY);
+
+        animatorSet.setDuration(2500);
+
+        animatorSet.start();
     }
 
 }
