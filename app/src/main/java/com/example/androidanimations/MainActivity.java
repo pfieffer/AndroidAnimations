4b1ba6802309ccc574deaf49e56d47ec25d8b03f
pfieffer <cbpmgr@gmail.com>
2019-02-28 20:48:44 +0545

    Listen for animation events

--- app/src/main/java/com/example/androidanimations/MainActivity.java
+++ app/src/main/java/com/example/androidanimations/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.androidanimations;
 
-import android.animation.AnimatorSet;
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.AnticipateOvershootInterpolator;
 import android.view.animation.BounceInterpolator;
-import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "AnimationEvents";
     private ImageView imageView;
     private RelativeLayout canvas;
 
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView =  findViewById(R.id.faceIcon);
+        imageView = findViewById(R.id.faceIcon);
         canvas = findViewById(R.id.animationCanvas);
     }
 
@@ -34,6 +35,37 @@ public class MainActivity extends AppCompatActivity {
                 .setDuration(1000);
 
         animator.setInterpolator(new BounceInterpolator()); //AnticipateOvershootInterpolator, LinearInterpolator
+
+        animator.setRepeatCount(2); //original iteration+ 2 times
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                Log.i(TAG, "onAnimationUpdate: "+valueAnimator.getAnimatedValue());
+            }
+        });
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                Log.i(TAG, "onAnimationStart: ");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                Log.i(TAG, "onAnimationEnd: ");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                Log.i(TAG, "onAnimationRepeat: ");
+            }
+        });
 
         animator.start();
     }
