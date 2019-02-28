c777557820cead2d1721ed4abcc3a2215abf3baf
pfieffer <cbpmgr@gmail.com>
2019-02-28 20:29:22 +0545

    Implement ObjectAnimator

--- app/src/main/java/com/example/androidanimations/MainActivity.java
+++ app/src/main/java/com/example/androidanimations/MainActivity.java
@@ -1,56 +1,33 @@
 package com.example.androidanimations;
 
-import android.animation.ValueAnimator;
-import android.graphics.drawable.AnimationDrawable;
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ScrollView mScroll;
-    private TextView mLog;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//      Initialize the logging components
-        mScroll =  findViewById(R.id.scrollLog);
-        mLog =  findViewById(R.id.tvLog);
-        mLog.setText("");
-
+        imageView =  findViewById(R.id.faceIcon);
     }
 
-    public void onRunBtnClick(View v) {
-        ValueAnimator animator = ValueAnimator.ofFloat(1f, 20f)
-                .setDuration(2000);
+    public void onButtonClick(View v) {
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(imageView, "scaleX", 1f, 2f)
+                .setDuration(1000);
 
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                displayMessage("timeStamp: "+valueAnimator.getCurrentPlayTime() + ", value:"+ valueAnimator.getAnimatedValue());
-            }
-        });
+        animatorX.start();
 
-        animator.setRepeatCount(1); // repeats the animation again, ie run for a total of two times
-//        animator.setRepeatCount(ValueAnimator.INFINITE); //infinite animation
-        //animator.setRepeatMode(ValueAnimator.REVERSE);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageView, "scaleY", 1f, 2f)
+                .setDuration(1000);
 
-        animator.start();
+        animatorY.start();
     }
 
-    public void onClearBtnClick(View v) {
-        mLog.setText("");
-        mScroll.scrollTo(0, mScroll.getBottom());
-    }
-
-    public void displayMessage(String message) {
-        mLog.append(message + "\n");
-        mScroll.scrollTo(0, mScroll.getBottom());
-    }
 }
